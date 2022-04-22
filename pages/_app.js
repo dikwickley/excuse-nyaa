91af349e3cc853ae57d1884d9f528dd2c9efe559
dikwickley <sprx077@gmail.com>
2022-04-22 21:23:06 +0530

    done ui

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,26 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { AwesomeButton } from "react-awesome-button";
+import "react-awesome-button/dist/styles.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className="bg-slate-700 h-[100vh] w-[100w] flex flex-col justify-center items-center">
+      <AwesomeButton
+        type="secondary"
+        style={{
+          "--button-secondary-color": "#fffc6c",
+          "--button-secondary-color-dark": "#b9b500",
+          "--button-secondary-color-light": "#6c6a00",
+          "--button-secondary-color-hover": "#fffb3e",
+          "--button-secondary-border": "none",
+        }}
+        size="large"
+        ripple={true}
+      >
+        <div className="font-pixel">find excuse</div>
+      </AwesomeButton>
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        pixel: ["'Press Start 2P'", "cursive"],
+      },
+    },
+  },
+  plugins: [],
+};
